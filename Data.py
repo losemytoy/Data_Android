c899f1f3ed6ee39c0e34771447119d112ce9e377
林 <2391660155@qq.com>
2020-01-15 15:28:51 +0800

    连接guzzi数据库

--- Data.py
+++ Data.py
@@ -9,13 +9,10 @@ import pymssql
 # 定义函数抓取每页前30条商品信息
 def crow_first(keyword,n):
     # 构造每一页的url变化
-    url = 'https://search.jd.com/Search?keyword='+str(keyword)+'&enc=utf-8&page=%d'+str(2 * n - 1)
-    # url = 'https://search.jd.com/Search?keyword='+str(keyword)+'&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&cid2=653&cid3=655&page=%d'+str(2 * n - 1)
+    url = 'http://search.jd.com/Search?keyword='+str(keyword)+'&enc=utf-8&page=%d'+str(2 * n - 1)
     head = {'authority': 'search.jd.com',
             'method': 'GET',
-            # 'path': '/s_new.php?keyword=%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&wq=%E6%89%8B%E6%9C%BA&cid2=653&cid3=655&page=4&s=84&scrolling=y&log_id=1529828108.22071&tpl=3_M&show_items=7651927,7367120,7056868,7419252,6001239,5934182,4554969,3893501,7421462,6577495,26480543553,7345757,4483120,6176077,6932795,7336429,5963066,5283387,25722468892,7425622,4768461',
             'scheme': 'https',
-            # 'referer': 'https://search.jd.com/Search?keyword=%E6%89%8B%E6%9C%BA&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&wq=%E6%89%8B%E6%9C%BA&cid2=653&cid3=655&page=3&s=58&click=0',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
             'x-requested-with': 'XMLHttpRequest',
             'Cookie': 'qrsc=3; pinId=RAGa4xMoVrs; xtest=1210.cf6b6759; ipLocation=%u5E7F%u4E1C; _jrda=5; TrackID=1aUdbc9HHS2MdEzabuYEyED1iDJaLWwBAfGBfyIHJZCLWKfWaB_KHKIMX9Vj9_2wUakxuSLAO9AFtB2U0SsAD-mXIh5rIfuDiSHSNhZcsJvg; shshshfpa=17943c91-d534-104f-a035-6e1719740bb6-1525571955; shshshfpb=2f200f7c5265e4af999b95b20d90e6618559f7251020a80ea1aee61500; cn=0; 3AB9D23F7A4B3C9B=QFOFIDQSIC7TZDQ7U4RPNYNFQN7S26SFCQQGTC3YU5UZQJZUBNPEXMX7O3R7SIRBTTJ72AXC4S3IJ46ESBLTNHD37U; ipLoc-djd=19-1607-3638-3638.608841570; __jdu=930036140; user-key=31a7628c-a9b2-44b0-8147-f10a9e597d6f; areaId=19; __jdv=122270672|direct|-|none|-|1529893590075; PCSYCityID=25; mt_xid=V2_52007VwsQU1xaVVoaSClUA2YLEAdbWk5YSk9MQAA0BBZOVQ0ADwNLGlUAZwQXVQpaAlkvShhcDHsCFU5eXENaGkIZWg5nAyJQbVhiWR9BGlUNZwoWYl1dVF0%3D; __jdc=122270672; shshshfp=72ec41b59960ea9a26956307465948f6; rkv=V0700; __jda=122270672.930036140.-.1529979524.1529984840.85; __jdb=122270672.1.930036140|85.1529984840; shshshsID=f797fbad20f4e576e9c30d1c381ecbb1_1_1529984840145'
@@ -29,9 +26,9 @@ def crow_first(keyword,n):
     # 定位到每一个商品标签li
     datas = html1.xpath('//li[contains(@class,"gl-item")]')
     # 将抓取的结果保存到本地CSV文件中
-    with open('JD_DATA.csv', 'a', newline='', encoding='utf-8')as f:
+    with open('JD.csv', 'a', newline='', encoding='utf-8')as f:
         write = csv.writer(f)
-        write.writerow(['商品名','价格','图片'])
+        # write.writerow(['商品名','价格','图片'])
         p_pic=[]
         for data in datas:
             i=i+1
@@ -40,11 +37,8 @@ def crow_first(keyword,n):
             if not detail_url.startswith("http"):
                 detail_url = "https:" + detail_url
                 ret = requests.get(detail_url).text
-                # print(">>>", type(ret))
-            # try:
-                # 捕获异常，当正则表达式没有获取到对象时，为了让程序正常运行
+
                 res = re.findall(r"imageList\:(.*?jpg\"\])", ret)[0]
-                # 字符串嵌套列表 处理成列表，再遍历
                 imageList = res.replace('[', '').replace(']', '').replace('"', '').split(',')
                 r=0
                 for image in imageList:
@@ -56,16 +50,10 @@ def crow_first(keyword,n):
                             image = image.lstrip()
                         # 拼接成完整img_url
                         fullImageUrl = "https://img11.360buyimg.com/n1/" + image
-                        # print(fullImageUrl)
-                        # print(image_title)
                         p_pic= fullImageUrl
                         r=r+1
                     else:
                         continue
-                # print(p_pic)
-            # except:
-            #     ''' 出现异常则打印，不阻止程序奔溃 '''
-            #     print('图片加载异常！')
             p_price = data.xpath('div/div[@class="p-price"]/strong/i/text()')
             p_comment = data.xpath('div/div[5]/strong/a/text()')
             p_name = data.xpath('div/div[@class="p-name p-name-type-2"]/a/em')
@@ -77,7 +65,7 @@ def crow_first(keyword,n):
     f.close()
 
 
-# 定义函数抓取每页后30条商品信息
+# # 定义函数抓取每页后30条商品信息
 def crow_last(keyword,n):
     # 获取当前的Unix时间戳，并且保留小数点后5位
     a = time.time()
@@ -98,7 +86,7 @@ def crow_last(keyword,n):
     r.encoding = 'utf-8'
     html1 = etree.HTML(r.text)
     datas = html1.xpath('//li[contains(@class,"gl-item")]')
-    with open('JD_DATA.csv', 'a', newline='', encoding='utf-8')as f:
+    with open('JD.csv', 'a', newline='', encoding='utf-8')as f:
         write = csv.writer(f)
         # write.writerow(['商品名', '价格', '图片'])
         p_pic = []
@@ -154,7 +142,7 @@ def Save_Database():
         raise Exception('数据库连接失败！')
 
     # 4.修改数据
-    reader = csv.reader(open(r'JD_DATA.csv', 'r',encoding='utf-8'))
+    reader = csv.reader(open(r'JD.csv', 'r',encoding='utf-8'))
     for item in reader:
         # script = "insert into Product(ProName,Price,Img_url) values ('{0}','{1}','{2}')".format(item[0], item[1],item[2])#此处为数据表信息，更改数据表 需修改
         script = "insert into goods(goodsname,goodsprice,imgurl) values ('{0}','{1}','{2}')".format(item[0], item[1], item[2])
